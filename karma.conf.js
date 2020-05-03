2afb022e6b35e5de5d15e41404952daadd2df63f
Robert Mennell <skatcat31@gmail.com>
2020-05-03 02:17:50 -0700

    fix renaming of generated files
    which is weird because this generated to the right folder

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/hagman'),
+      dir: require('path').join(__dirname, './coverage/hangman'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
