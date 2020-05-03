be0885c5c1411f50e5c5eaafb1032399e34d5ad8
Robert Mennell <skatcat31@gmail.com>
2020-05-03 03:25:14 -0700

    update testing to jest and testing library
    this allows me to write tests ina  way a user would use the website
    it also allows for a much more streamlined test setup

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    preset: 'jest-preset-angular',
+    setupFilesAfterEnv: [
+        './jest.setup.ts'
+    ]
+};
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,10 @@
-import { TestBed, async } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
+import { render } from '@testing-library/angular';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  }));
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'hangman'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('hangman');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('hangman app is running!');
+  it('should create the app', async () => {
+    const { getByText } = await render(AppComponent);
+    expect(getByText('Hangman')).toBeInTheDocument();
   });
 });
