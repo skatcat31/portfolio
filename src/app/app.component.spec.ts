e022a2011ec23a4f25a33b63417c098d4cb3ac2a
Robert Mennell <skatcat31@gmail.com>
2020-05-05 02:20:15 -0700

    Mke tests more copy paste friendly

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,12 @@
 import { render } from '@testing-library/angular';
 import { AppComponent } from './app.component';
 
+const Component = AppComponent;
+
 describe('AppComponent', () => {
 
   it('should create the app', async () => {
-    const { getByText } = await render(AppComponent);
+    const { getByText } = await render(Component);
     expect(getByText('Hangman')).toBeInTheDocument();
   });
 });
