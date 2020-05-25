3ab093e9c932dff98abe6b3809173a37df88ef6a
Robert Mennell <skatcat31@gmail.com>
2020-05-25 09:14:02 -0700

    make ids on inputs unique

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
@@ -6,9 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent implements OnInit {
-
   constructor() { }
 
+  @Input() id: string;
   ngOnInit(): void {
   }
 
