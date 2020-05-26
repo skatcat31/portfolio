d1670d203cb8770e2b7c073e6811a932b7079be0
Robert Mennell <skatcat31@gmail.com>
2020-05-25 21:18:07 -0700

    full coverage on sidebar nav

--- src/app/sidebar/sidebar.component.spec.ts
+++ src/app/sidebar/sidebar.component.spec.ts
@@ -1,25 +1,83 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { render, fireEvent, waitFor } from '@testing-library/angular';
 import { SidebarComponent } from './sidebar.component';
+import { RouterTestingModule } from '@angular/router/testing';
 
-describe('SidebarComponent', () => {
-  let component: SidebarComponent;
-  let fixture: ComponentFixture<SidebarComponent>;
+import '@testing-library/jest-dom';
+import { A11yModule } from '@angular/cdk/a11y';
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ SidebarComponent ]
-    })
-    .compileComponents();
-  }));
+const Component = SidebarComponent;
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(SidebarComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+const renderComp = (mobile = false) => render(Component, {
+  imports: [
+    [
+      RouterTestingModule.withRoutes([]),
+      A11yModule
+    ]
+  ],
+  componentProperties: { mobile }
+});
+
+describe('AppComponent', () => {
+  it('should create the desktop sidebar', async () => {
+    const { container, getByText } = await renderComp();
+    expect(container.querySelector('nav')).toBeInTheDocument();
+    expect(container.querySelectorAll('.cdk-focus-trap-anchor')).toHaveLength(0);
+    expect(container.querySelector(`button`)).not.toBeInTheDocument();
+
+    const links = container.querySelectorAll('a');
+    expect(links).toHaveLength(2);
+    expect(links[0]).not.toHaveAttribute('aria-hidden');
+    expect(links[0]).toBe(getByText('About Me'));
+    expect(links[1]).not.toHaveAttribute('aria-hidden');
+    expect(links[1]).toBe(getByText('Technical'));
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should create the mobile sidebar', async () => {
+    const mobile = true;
+    const { container, getByText } = await renderComp(mobile);
+
+    // gather things to test against
+    const button = container.querySelector(`button`);
+    const links = container.querySelectorAll('a');
+
+    // test the existance
+    expect(button).toBeInTheDocument();
+    expect(getByText('Navigation')).toBe(button);
+    // check that the focustrap exists
+    expect(container.querySelectorAll('.cdk-focus-trap-anchor')).toHaveLength(2);
+
+    expect(links).toHaveLength(2);
+    expect(links[0]).toBe(getByText('About Me'));
+    expect(links[1]).toBe(getByText('Technical'));
+    expect(links[0]).toHaveAttribute('aria-hidden', 'true');
+    expect(links[0]).toHaveAttribute('tabindex', '-1');
+    expect(links[1]).toHaveAttribute('aria-hidden', 'true');
+    expect(links[1]).toHaveAttribute('tabindex', '-1');
+
+    // test the behaviour
+    fireEvent.click(button);
+    await waitFor(() => expect(links[0]).toHaveAttribute('aria-hidden', 'false'));
+    expect(links[0]).toBe(getByText('About Me'));
+    expect(links[1]).toBe(getByText('Technical'));
+    expect(links[1]).toHaveAttribute('aria-hidden', 'false');
+    expect(links[0]).toHaveAttribute('tabindex', '0');
+    expect(links[1]).toHaveAttribute('aria-hidden', 'false');
+    expect(links[1]).toHaveAttribute('tabindex', '0');
+
+    // navigate
+    fireEvent.click(links[0]);
+    await waitFor(() => expect(links[0]).toHaveAttribute('aria-hidden', 'true'));
+    expect(links[0]).toBe(getByText('About Me'));
+    expect(links[1]).toBe(getByText('Technical'));
+    expect(links[0]).toHaveAttribute('aria-hidden', 'true');
+    expect(links[0]).toHaveAttribute('tabindex', '-1');
+    expect(links[1]).toHaveAttribute('aria-hidden', 'true');
+    expect(links[1]).toHaveAttribute('tabindex', '-1');
+
+    // close with button
+    fireEvent.click(button);
+    await waitFor(() => expect(links[0]).toHaveAttribute('aria-hidden', 'false'));
+    fireEvent.click(button);
+    await waitFor(() => expect(links[0]).toHaveAttribute('aria-hidden', 'true'));
   });
 });
