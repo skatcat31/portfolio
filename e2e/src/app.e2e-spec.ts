6b7299df51b098fd8e771e7f0fb47302ae4966d9
Robert Mennell <skatcat31@gmail.com>
2020-05-25 22:03:03 -0700

    remove all isntance of hangman

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('hangman app is running!');
+    expect(page.getTitleText()).toEqual('portfolio app is running!');
   });
 
   afterEach(async () => {
