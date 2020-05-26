312cec4b0866e4ffd01661677c37fd32df952042
Robert Mennell <skatcat31@gmail.com>
2020-05-25 21:36:34 -0700

    app component tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -5,19 +5,31 @@ import { RouterTestingModule } from '@angular/router/testing';
 import '@testing-library/jest-dom';
 import { Route } from './app-routing.module';
 import { LandingComponent } from './landing/landing.component';
+import { A11yModule } from '@angular/cdk/a11y';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 const Component = AppComponent;
+
+const renderComp = () => render(Component, {
+  imports: [
+    RouterTestingModule.withRoutes([
+      new Route('', LandingComponent)
+    ]),
+    A11yModule,
+  ],
+  declarations: [
+    SidebarComponent
+  ]
+});
 
 describe('AppComponent', () => {
 
   it('should create the app', async () => {
-    const { getByText } = await render(Component, {
-      imports: [
-        RouterTestingModule.withRoutes([
-          new Route('', LandingComponent)
-      ])
-      ]
-    });
-    expect(getByText('Hangman')).toBeInTheDocument();
+    const { getByText, getByTitle, getByAltText, getAllByText } = await renderComp();
+    expect(getByText('Robert Mennell')).toBeInTheDocument();
+    expect(getByTitle('Robert\'s LinkedIn Profile')).toBeInTheDocument();
+    expect(getByAltText('Robert\'s GitHub Profile')).toBeInTheDocument();
+    // renders both the mobile and dekstop navigation, just confirm About Me link is present
+    expect(getAllByText('About Me')).toHaveLength(2);
   });
 });
